abfa93a04d698c2aaf6536b2ba2943638b4ed342
dekim1028 <daeun235194@gmail.com>
2020-12-22 12:26:53 +0900

    Update structure

---
+++ src/container/MainContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from '../component/Main';
+
+const MainContainer = () => {
+	return <Main />;
+};
+
+export default MainContainer;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainContainer from '../container/MainContainer';
+
+const MainPage = () => {
+	return <MainContainer />;
+};
+
+export default MainPage;
