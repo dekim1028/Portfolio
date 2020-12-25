36f065fc787b00aae8a00a6119c53f262808b38d
dekim1028 <daeun235194@gmail.com>
2020-12-25 17:31:32 +0900

    Implement Menu component

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import cn from 'classnames';
-import { CgPushChevronDown } from 'react-icons/cg';
+import { CgPushChevronDown, CgMail } from 'react-icons/cg';
+import {
+	FcVoicePresentation,
+	FcBriefcase,
+	FcList,
+	FcCommandLine,
+} from 'react-icons/fc';
 
 const MenuBlock = styled.div`
 	width: 100%;
@@ -55,32 +61,163 @@ const DownBtn = styled(CgPushChevronDown)`
 `;
 
 const MenuList = styled.div`
-	width: 90%;
+	width: 600px;
 	height: 90px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	margin: 0 auto;
 	background: rgba(255, 255, 255, 0.3);
-	border-radius: 25px;
+	border-radius: 20px;
 	padding: 10px;
+
+	@media (max-width: 768px) {
+		width: 90%;
+	}
 `;
 
 const MenuItemBlock = styled.div`
-	display: inline-block;
+	margin: 0 20px;
+
+	@media (max-width: 768px) {
+		margin: 0 5px;
+	}
 `;
 
-const MenuItemImage = styled.img``;
+const MenuItemImage = styled.div`
+	width: 60px;
+	height: 60px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: rgba(255, 255, 255, 0.4);
+	border-radius: 10px;
+	font-size: 50px;
+	transition: 0.3s;
 
-const MenuItemText = styled.h1`
-	margin: 0;
+	&:hover {
+		background: rgba(255, 255, 255, 0.6);
+	}
+
+	@media (max-width: 768px) {
+		width: 50px;
+		height: 50px;
+	}
 `;
 
-const Menu = ({ showMenu, onShowMenu }) => {
+const SpeechBubble = styled.div`
+	width: 100px;
+	position: absolute;
+	top: 10px;
+	transform: translate(-20px, 0px);
+	padding: 5px;
+	background: rgba(255, 255, 255, 0.8);
+	border-radius: 5px;
+	text-align: center;
+	color: black;
+	font-size: 10pt;
+	display: none;
+
+	@media (max-width: 768px) {
+		transform: translate(-25px, 0px);
+	}
+
+	&:before {
+		width: 0;
+		height: 0;
+		position: absolute;
+		bottom: -74%;
+		left: 25px;
+		transform: translate(15px, 3px);
+		content: ' ';
+		border: solid transparent;
+		border-color: rgba(56, 77, 157, 0);
+		border-top-color: rgba(255, 255, 255, 0.8);
+		border-width: 11px;
+		pointer-events: none;
+	}
+
+	&.multiline {
+		top: -6px;
+		&:before {
+			bottom: -45%;
+		}
+	}
+
+	&.show {
+		display: block;
+	}
+`;
+
+const Menu = ({
+	showMenu,
+	speechBubble,
+	onShowMenu,
+	onShowSpeechBubble,
+	onHideSpeechBubble,
+}) => {
 	return (
 		<MenuBlock className={cn({ showMenu })}>
 			<DownBtn className={cn({ showMenu })} onClick={onShowMenu} />
 			<MenuList>
-				<MenuItemBlock>
-					<MenuItemImage />
-					<MenuItemText></MenuItemText>
+				<MenuItemBlock
+					onMouseOver={() => onShowSpeechBubble('myInfo')}
+					onMouseOut={() => onHideSpeechBubble('myInfo')}
+				>
+					<SpeechBubble className={cn({ show: speechBubble.myInfo })}>
+						Introduce
+					</SpeechBubble>
+					<MenuItemImage>
+						<FcVoicePresentation />
+					</MenuItemImage>
+				</MenuItemBlock>
+				<MenuItemBlock
+					onMouseOver={() => onShowSpeechBubble('stack')}
+					onMouseOut={() => onHideSpeechBubble('stack')}
+				>
+					<SpeechBubble className={cn({ show: speechBubble.stack })}>
+						Skill
+					</SpeechBubble>
+					<MenuItemImage>
+						<FcList />
+					</MenuItemImage>
+				</MenuItemBlock>
+				<MenuItemBlock
+					onMouseOver={() => onShowSpeechBubble('career')}
+					onMouseOut={() => onHideSpeechBubble('career')}
+				>
+					<SpeechBubble
+						className={cn({ show: speechBubble.career, multiline: true })}
+					>
+						Work Experience
+					</SpeechBubble>
+					<MenuItemImage>
+						<FcBriefcase />
+					</MenuItemImage>
+				</MenuItemBlock>
+				<MenuItemBlock
+					onMouseOver={() => onShowSpeechBubble('portfolio')}
+					onMouseOut={() => onHideSpeechBubble('portfolio')}
+				>
+					<SpeechBubble
+						className={cn({ show: speechBubble.portfolio, multiline: true })}
+					>
+						Other Experience
+					</SpeechBubble>
+					<MenuItemImage>
+						<FcCommandLine />
+					</MenuItemImage>
+				</MenuItemBlock>
+				<MenuItemBlock
+					onMouseOver={() => onShowSpeechBubble('contact')}
+					onMouseOut={() => onHideSpeechBubble('contact')}
+				>
+					<SpeechBubble className={cn({ show: speechBubble.contact })}>
+						Contact
+					</SpeechBubble>
+					<MenuItemImage>
+						<CgMail />
+					</MenuItemImage>
 				</MenuItemBlock>
 			</MenuList>
 		</MenuBlock>
--- src/container/MenuContainer.js
+++ src/container/MenuContainer.js
@@ -3,12 +3,41 @@ import Menu from '../component/Menu';
 
 const MenuContainer = () => {
 	const [showMenu, setShowMenu] = useState(false);
+	const [speechBubble, setSpeechBubble] = useState({
+		myInfo: false,
+		career: false,
+		stack: false,
+		portfolio: false,
+		contact: false,
+	});
 
 	const onShowMenu = () => {
 		setShowMenu(!showMenu);
 	};
 
-	return <Menu showMenu={showMenu} onShowMenu={onShowMenu} />;
+	const onShowSpeechBubble = (id) => {
+		setSpeechBubble({
+			...speechBubble,
+			[id]: true,
+		});
+	};
+
+	const onHideSpeechBubble = (id) => {
+		setSpeechBubble({
+			...speechBubble,
+			[id]: false,
+		});
+	};
+
+	return (
+		<Menu
+			showMenu={showMenu}
+			speechBubble={speechBubble}
+			onShowMenu={onShowMenu}
+			onShowSpeechBubble={onShowSpeechBubble}
+			onHideSpeechBubble={onHideSpeechBubble}
+		/>
+	);
 };
 
 export default MenuContainer;
